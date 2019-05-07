2f4227a0d9c840b86999f3e6d9132109793a2637
Unknown <randyjkeller@gmail.com>
2019-05-06 21:21:36 -0500

    Updated for date/time validation

--- tankinv_sappi_controller.reproc.py
+++ tankinv_sappi_controller.reproc.py
@@ -1,34 +1,56 @@
 #!/bin/python
+
+# check for modules
 import datetime
 import time
 
-def gather_info():
-    #validate User Input Function
-    date_string=input("Enter Date (Format: YYYY/MM/DD) ")
-    date_format='%Y/%m/%d'
-    time_string=input("Enter Time (Format: HH:MM:SS) ")
-    time_format='%H:%M:%S'
 
-    # Validate Date
-    try:
-        date_obj=datetime.datetime.strptime(date_string, date_format)
-    except ValueError:
-        print("Incorrect date format, should be YYYY/MM/DD")
-        gather_info()
-    # Validate Time
-    try:
-        time_obj=datetime.datetime.strptime(time_string, time_format)
-    except ValueError:
-        print("Incorrect time format, should be HH:MM:SS")
-        gather_info()
+def check_date():   # add both functions
+    print("Enter Date (Format: YYYY/MM/DD): ")
 
-    return(date_string,time_string)
+    while True:
+        try:
+            date_format = '%Y/%m/%d'
+            date_string = str(input())
+            date_obj = datetime.datetime.strptime(date_string, date_format)
+        except ValueError:
+            print("Incorrect date format, should be YYYY/MM/DD:")
+            continue
+        else:
+            break
+    return date_string
 
-while True:
-    date_string,time_string = gather_info()
+
+def check_time():
+    print("Enter time (Format: HH:MM:SS): ")
+
+    while True:
+        try:
+            time_format='%H:%M:%S'
+            time_string = str(input())
+            time_obj=time.strptime(time_string, time_format)
+        except ValueError:
+            print("Incorrect time format, should be HH:MM:SS:")
+            continue
+        else:
+            break
+    return time_string
+
+
+if __name__ == '__main__':
+    date_string = check_date()
+    time_string = check_time()
+
+    # Calculate time
     start_time=time.mktime(time.strptime('2018/10/14 23:31:01', '%Y/%m/%d %H:%M:%S'))
-    start_time2=time.mktime(time.strptime(date_string + " " +  time_string, '%Y/%m/%d %H:%M:%S'))
-    # Use Original date and time as check for calculations
-    print ("Original time: 2018/10/14: " + str(start_time)) # correct value for org date/time 1539577861.0
-    print ("New time:                  " + str(start_time2))
-    print (" ")
+    start_time2=time.mktime(time.strptime(date_string + " " + time_string, '%Y/%m/%d %H:%M:%S'))
+
+print ("Date is " + date_string)
+print ("Time is " + time_string)
+
+# Use Original date and time as check for calculations
+print ("Original time: 2018/10/14: " + str(start_time))  # correct value for org date/time 1539577861.0
+print ("New time:                  " + str(start_time2))
+print (" ")
+
+
